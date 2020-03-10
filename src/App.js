da3ab39945a6c4f128d27881f0fb56b78a0c9625
nickzylstra <nick.zylstra@gmail.com>
2020-03-09 17:48:32 -0700

    Process energy data

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import './App.css';
 import populationData from './data/populations.json'
+import energyData from './data/energy.json'
 
 function processPopulationData(input, output = {}) {
+  console.log(`Countries in population data: ${input.length}`);
   input.forEach((country) => {
     const name = country.Country;
-    output[name] = {
-      code: country.Country_Code,
+    const code = country.Country_Code;
+    output[code] = {
+      name,
       populations: {},
     };
 
     Object.keys(country).forEach((key) => {
       const yearMatch = key.match(/year_(.*)/i);
-      debugger;
       if (yearMatch) {
         const year = yearMatch[1];
-        output[name].populations[year] = country[key];
+        output[code].populations[year] = country[key];
       }
     })
   })
@@ -23,9 +25,28 @@ function processPopulationData(input, output = {}) {
   return output;
 }
 
+function processEnergyData(input, output = {}) {
+  console.log(`Countries in energy data: ${input.length}`);
+
+  input.forEach((country) => {
+    const code = country.iso;
+    if (!output[code]) output[code] = {};
+    output[code].energyConsumptions = {};
+
+    country.data.forEach((yearData) => {
+      const { date, value } = yearData;
+      const year = (new Date(date)).getFullYear();
+      output[code].energyConsumptions[year] = value === '--' ? null : value;
+    });
+  });
+
+  return output;
+}
+
 function App() {
   const data = processPopulationData(populationData);
-  
+  processEnergyData(energyData, data);
+
   return (
     <div className="App">
       {JSON.stringify(data)}
