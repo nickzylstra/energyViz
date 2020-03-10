c2138407a20ea6c1416f0ebca8a7c11e61011e98
nickzylstra <nick.zylstra@gmail.com>
2020-03-09 19:44:04 -0700

    Calculate energy per capitas

--- src/importData.js
+++ src/importData.js
@@ -2,11 +2,11 @@ import populationData from './data/populations.json'
 import energyData from './data/energy.json'
 
 function processPopulationData(input, output = {}) {
-  console.log(`Countries in population data: ${input.length}`);
+  // console.log(`Countries in population data: ${input.length}`);
   input.forEach((country) => {
     const name = country.Country;
     const code = country.Country_Code;
-    output[code] = {
+    output.countries[code] = {
       name,
       populations: {},
     };
@@ -15,7 +15,11 @@ function processPopulationData(input, output = {}) {
       const yearMatch = key.match(/year_(.*)/i);
       if (yearMatch) {
         const year = yearMatch[1];
-        output[code].populations[year] = country[key];
+
+        if (!output.years[year]) output.years[year] = { populationCount: 0 };
+        output.years[year].populationCount += 1;
+
+        output.countries[code].populations[year] = country[key];
       }
     })
   })
@@ -24,24 +28,46 @@ function processPopulationData(input, output = {}) {
 }
 
 function processEnergyData(input, output = {}) {
-  console.log(`Countries in energy data: ${input.length}`);
-
+  // console.log(`Countries in energy data: ${input.length}`);
   input.forEach((country) => {
     const code = country.iso;
-    if (!output[code]) output[code] = {};
-    output[code].energyConsumptions = {};
+    if (!output.countries[code]) output.countries[code] = {};
+    output.countries[code].energyConsumptions = {};
 
     country.data.forEach((yearData) => {
       const { date, value } = yearData;
       const year = (new Date(date)).getFullYear();
-      output[code].energyConsumptions[year] = value === '--' ? null : value;
+
+      if (!output.years[year]) output.years[year] = { energyCount: 0 };
+      if (!output.years[year].energyCount) output.years[year].energyCount = 0;
+      output.years[year].energyCount += 1;
+
+      output.countries[code].energyConsumptions[year] = value === '--' ? null : value;
     });
   });
 
   return output;
 }
 
-const data = processPopulationData(populationData);
+function calculateEnergyPerCapita(data) {
+  Object.values(data.countries).forEach((country) => {
+    country.energyPerCapitas = {};
+    Object.keys(data.years).forEach((year) => {
+      const population = country.populations && country.populations[year];
+      const energy = country.energyConsumptions && country.energyConsumptions[year];
+      const kWHperBkWH = 1000000000;
+      const energyPerCapita = energy * kWHperBkWH / population;
+      country.energyPerCapitas[year] = energyPerCapita;
+    });
+  });
+}
+
+const data = {
+  years: {},
+  countries: {},
+};
+processPopulationData(populationData, data);
 processEnergyData(energyData, data);
+calculateEnergyPerCapita(data);
 
 export default data;
