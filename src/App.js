dab34e00db9de41088a79c3d52d858b6270a8eb9
nickzylstra <nick.zylstra@gmail.com>
2020-03-09 17:57:17 -0700

    Move data processing into separate module

--- src/App.js
+++ src/App.js
@@ -1,51 +1,9 @@
 import React from 'react';
 import './App.css';
-import populationData from './data/populations.json'
-import energyData from './data/energy.json'
+import data from './importData';
 
-function processPopulationData(input, output = {}) {
-  console.log(`Countries in population data: ${input.length}`);
-  input.forEach((country) => {
-    const name = country.Country;
-    const code = country.Country_Code;
-    output[code] = {
-      name,
-      populations: {},
-    };
-
-    Object.keys(country).forEach((key) => {
-      const yearMatch = key.match(/year_(.*)/i);
-      if (yearMatch) {
-        const year = yearMatch[1];
-        output[code].populations[year] = country[key];
-      }
-    })
-  })
-
-  return output;
-}
-
-function processEnergyData(input, output = {}) {
-  console.log(`Countries in energy data: ${input.length}`);
-
-  input.forEach((country) => {
-    const code = country.iso;
-    if (!output[code]) output[code] = {};
-    output[code].energyConsumptions = {};
-
-    country.data.forEach((yearData) => {
-      const { date, value } = yearData;
-      const year = (new Date(date)).getFullYear();
-      output[code].energyConsumptions[year] = value === '--' ? null : value;
-    });
-  });
-
-  return output;
-}
 
 function App() {
-  const data = processPopulationData(populationData);
-  processEnergyData(energyData, data);
 
   return (
     <div className="App">
