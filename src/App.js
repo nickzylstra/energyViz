b9f3179c791a28914b4a72a09b72ea7d2b1d6c7d
nickzylstra <nick.zylstra@gmail.com>
2020-03-09 17:12:16 -0700

    Parse population data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import populationData from './data/populations.json'
+
+function processPopulationData(input, output = {}) {
+  input.forEach((country) => {
+    const name = country.Country;
+    output[name] = {
+      code: country.Country_Code,
+      populations: {},
+    };
+
+    Object.keys(country).forEach((key) => {
+      const yearMatch = key.match(/year_(.*)/i);
+      debugger;
+      if (yearMatch) {
+        const year = yearMatch[1];
+        output[name].populations[year] = country[key];
+      }
+    })
+  })
+
+  return output;
+}
 
 function App() {
+  const data = processPopulationData(populationData);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {JSON.stringify(data)}
     </div>
   );
 }
