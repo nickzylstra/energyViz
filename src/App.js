84cedef67dc0c8fff373f860711a727eda901f4e
nickzylstra <nick.zylstra@gmail.com>
2020-03-10 18:15:11 -0700

    Show sorted populations

--- src/App.js
+++ src/App.js
@@ -5,24 +5,26 @@ import Table from './Table';
 import data from './importData';
 
 const width = 600;
-const year = '2013';
-const scale = d3.scaleLinear()
-  // .domain([0, d3.max(data.years, ({ populationMax }) => populationMax)])
-  .domain([0, data.years[year].populationMax])
-  .range([0, width]);
+const year = '2016';
+function scalePop(year) {
+  return d3.scaleLinear()
+    .domain([0, d3.max(Object.entries(data.countries), ([name, { populations }]) => populations[year])])
+    .range([0, width]);
+} 
 
 
 function App() {
   useEffect(() => {
     const viz = d3.select('#viz');
-    viz.selectAll('div').data(Object.entries(data.countries)).join('div')
+    const vizData = Object.entries(data.countries).sort(([a, aStats], [b, bStats]) => bStats.populations[year] - aStats.populations[year]);
+    viz.selectAll('div').data(vizData).join('div')
       .style('background', 'steelblue')
       .style('border', '1px solid white')
       .style('font-size', 'small')
-      .style('color', 'white')
+      .style('color', 'black')
       .style('text-align', 'right')
       .style('padding', '3px')
-      .style('width', ([name, stats]) => `${scale(stats.populations[year])}px`)
+      .style('width', ([name, stats]) => `${scalePop(year)(stats.populations[year])}px`)
       .text(([name, stats]) => stats.populations[year]);
 
     return () => {
