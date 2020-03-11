6b998b6fb452933a80e8ed0e5272b035b43aabee
nickzylstra <nick.zylstra@gmail.com>
2020-03-10 17:00:46 -0700

    Display data in table

--- src/App.js
+++ src/App.js
@@ -1,13 +1,62 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import * as d3 from 'd3';
 import './App.css';
 import data from './importData';
 
+const width = 600;
+// const scale = d3.scaleLinear()
+//   .domain([0, d3.max(data)]);
 
 function App() {
+  useEffect(() => {
+    // const viz = d3.select('#viz');
+    // return () => {
+    //   viz.selectAll('div').data(Object.entries(data.countries)).join('div')
+    //     .style('background', 'steelblue')
+    //     .style('border', '1px solid white')
+    //     .style('font-size', 'small')
+    //     .style('color', 'white')
+    //     .style('text-align', 'right')
+    //     .style('padding', '3px')
+    //     .style('width', d => `${scale(d.populations['2017'])}px`)
+    //     .text((d) => d.name);
+    // };
+  });
 
   return (
     <div className="App">
-      {JSON.stringify(data)}
+      <div id='viz'>
+        <table>
+          <thead>
+            <tr>
+              <th>code</th>
+              <th>name</th>
+              <th>pop</th>
+              <th>energy</th>
+              <th>ePC</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Object.entries(data.countries).sort().map((country) => {
+              const [code, stats] = country;
+              const { name, energyPerCapitas, populations, energyConsumptions } = stats;
+              const ePCCount = energyPerCapitas.count;
+              const popCount = populations.count;
+              const energyCount = energyConsumptions.count;
+              
+              return (
+                <tr key={code}>
+                  <td>{code}</td>
+                  <td>{name}</td>
+                  <td>{popCount}</td>
+                  <td>{energyCount}</td>
+                  <td>{ePCCount}</td>
+                </tr>);
+            })}
+          </tbody>
+        </table>
+        {/* {JSON.stringify(data)} */}
+      </div>
     </div>
   );
 }
--- src/importData.js
+++ src/importData.js
@@ -1,25 +1,44 @@
 import populationData from './data/populations.json'
 import energyData from './data/energy.json'
 
+const exclusions = ['ARB','CEB','CSS','EAP','EAR','EAS','ECA','ECS','ECU','EMU', 'EUU', 'FCS', 'HIC', 'HITZ', 'HPC', 'IBD', 'IBT', 'IDA', 'IDB','IDX','LAC','LCN','LIC','LDC','LMC','LMY','LTE','MEA','MIC','MNA','NAC','OED','OSS','PRE','PSS','PST','REU','SAS','SSA','SSF','SST','TEA','TEC','TLA','TMN','TSA','TSS','UMC','WLD'];
+
+class Country {
+  constructor(name) {
+    this.name = name;
+    this.populations = { count: 0 };
+    this.energyConsumptions = { count: 0 };
+    this.energyPerCapitas = { count: 0 };
+  }
+}
+
+class Year {
+  constructor() {
+    this.populationMax = 0;
+    this.energyConsumptionMax = 0;
+    this.energyPerCapitaMax = 0;
+  }
+}
+
 function processPopulationData(input, output = {}) {
   // console.log(`Countries in population data: ${input.length}`);
   input.forEach((country) => {
     const name = country.Country;
     const code = country.Country_Code;
-    output.countries[code] = {
-      name,
-      populations: {},
-    };
+    if (exclusions.includes(code)) return;
+    output.countries[code] = new Country(name);
 
     Object.keys(country).forEach((key) => {
       const yearMatch = key.match(/year_(.*)/i);
       if (yearMatch) {
         const year = yearMatch[1];
+        const population = country[key];
 
-        if (!output.years[year]) output.years[year] = { populationCount: 0 };
-        output.years[year].populationCount += 1;
+        if (!output.years[year]) output.years[year] = new Year();
+        output.years[year].populationMax = Math.max(population, output.years[year].populationMax);
 
-        output.countries[code].populations[year] = country[key];
+        output.countries[code].populations[year] = population;
+        output.countries[code].populations.count += 1;
       }
     })
   })
@@ -31,18 +50,20 @@ function processEnergyData(input, output = {}) {
   // console.log(`Countries in energy data: ${input.length}`);
   input.forEach((country) => {
     const code = country.iso;
-    if (!output.countries[code]) output.countries[code] = {};
-    output.countries[code].energyConsumptions = {};
+    const name = country.name.split(',')[1].trim();
+    if (exclusions.includes(code)) return;
+    if (!output.countries[code]) output.countries[code] = new Country(name);
 
     country.data.forEach((yearData) => {
       const { date, value } = yearData;
+      if (value === '--') return;
       const year = (new Date(date)).getFullYear();
 
-      if (!output.years[year]) output.years[year] = { energyCount: 0 };
-      if (!output.years[year].energyCount) output.years[year].energyCount = 0;
-      output.years[year].energyCount += 1;
-
-      output.countries[code].energyConsumptions[year] = value === '--' ? null : value;
+      if (!output.years[year]) output.years[year] = new Year();
+      output.years[year].energyConsumptionMax = Math.max(value, output.years[year].energyConsumptionMax);
+      
+      output.countries[code].energyConsumptions[year] =  value;
+      output.countries[code].energyConsumptions.count += 1;
     });
   });
 
@@ -51,15 +72,21 @@ function processEnergyData(input, output = {}) {
 
 function calculateEnergyPerCapita(data) {
   Object.values(data.countries).forEach((country) => {
-    country.energyPerCapitas = {};
+    country.energyPerCapitas = { count: 0 };
+    if (country.populations.count === 0 || country.energyConsumptions.count === 0) return;
     Object.keys(data.years).forEach((year) => {
-      const population = country.populations && country.populations[year];
-      const energy = country.energyConsumptions && country.energyConsumptions[year];
+      const population = country.populations[year];
+      const energy = country.energyConsumptions[year];
       const kWHperBkWH = 1000000000;
       const energyPerCapita = energy * kWHperBkWH / population;
       country.energyPerCapitas[year] = energyPerCapita;
+      country.energyPerCapitas.count += 1;
     });
   });
+}
+
+function removeIncompleteDataSets(data) {
+  // data.countries = data.countries.filter((c) => );
 }
 
 const data = {
@@ -69,5 +96,6 @@ const data = {
 processPopulationData(populationData, data);
 processEnergyData(energyData, data);
 calculateEnergyPerCapita(data);
+removeIncompleteDataSets(data);
 
 export default data;
